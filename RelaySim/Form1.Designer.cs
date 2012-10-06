b16d3a529cbb2f4affd439a3ce545b09a37083ee
Jesse <jessenic@digiex.net>
2012-10-07 00:28:58 +0300

    Components can be added on the fly

--- RelaySim/Form1.Designer.cs
+++ RelaySim/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -41,9 +42,9 @@
             this.elementHost1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.elementHost1.Location = new System.Drawing.Point(12, 27);
+            this.elementHost1.Location = new System.Drawing.Point(201, 27);
             this.elementHost1.Name = "elementHost1";
-            this.elementHost1.Size = new System.Drawing.Size(925, 510);
+            this.elementHost1.Size = new System.Drawing.Size(736, 510);
             this.elementHost1.TabIndex = 0;
             this.elementHost1.Text = "elementHost1";
             this.elementHost1.Child = this.designControl1;
@@ -73,11 +74,21 @@
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(183, 511);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(949, 549);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.elementHost1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -97,6 +108,7 @@
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private DesignControl designControl1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- RelaySim/Form1.cs
+++ RelaySim/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RelaySim.Components;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,6 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Forms;
 
 namespace RelaySim
@@ -15,6 +17,37 @@ namespace RelaySim
         public Form1()
         {
             InitializeComponent();
+            listBox1.Items.Add(new ComponentListItem(typeof(Switch), "Switch"));
+            listBox1.Items.Add(new ComponentListItem(typeof(N), "N"));
+            listBox1.Items.Add(new ComponentListItem(typeof(L), "L"));
         }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            DesignControl dc = ((DesignControl)elementHost1.Child);
+            ContentControl cc = new ContentControl();
+            cc.Content = Activator.CreateInstance(((ComponentListItem)listBox1.SelectedItem).Control);
+            cc.Template = dc.FindResource("DesignerItemTemplate") as ControlTemplate;
+            Canvas.SetTop(cc, 10);
+            Canvas.SetLeft(cc, 50);
+            dc.Canvas.Children.Add(cc);
+        }
+    }
+    public class ComponentListItem
+    {
+        public ComponentListItem(Type control, string name)
+        {
+            this.Control = control;
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public Type Control { get; set; }
+
+        public string Name { get; set; }
     }
 }
